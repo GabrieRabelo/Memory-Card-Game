6de88c66542d18d698ed74616e61a9c76fbec850
Gabriel Rabelo <50777647+GabrieRabelo@users.noreply.github.com>
2019-06-18 07:44:24 -0300

    Add files via upload

---
+++ BlueJ Documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"Deck"},{"p":"<Unnamed>","l":"GameApp"},{"p":"<Unnamed>","l":"Highscore"},{"p":"<Unnamed>","l":"Utilities"}]
--- GameApp.java
+++ GameApp.java
@@ -11,12 +11,11 @@ import java.util.Scanner;
 
 public class GameApp{
     public static void main (String args[]) throws FileNotFoundException{
-        Utilities.startScreen();
+        Utilities.startScreen(); // Start screen of the game animation.
         Scanner in = new Scanner(System.in);
         int menuOption=0;
-        boolean load = false;
         do{
-            Utilities.window();
+            Utilities.window(); //Name of the game static
             System.out.println("            - Menu -\n");
             System.out.println("         1 - Novo Jogo");
             System.out.println("         2 - Carregar Jogo");
@@ -25,56 +24,50 @@ public class GameApp{
             menuOption = in.nextInt();
             
             switch(menuOption){
-                case 1: startGame(load); break;
-                case 2: load = true; startGame(load); break;
-                case 3: Highscore.menuHighscore(); break;
+                case 1: startGame(); break; 
+                case 3: Highscore.menuHighscore(); break; //best player atm
                 case 0: break;
                 default: System.out.println("Opção inválida");
             }
         }while(menuOption!=0);
     }
     
-    public static void startGame(boolean load) throws FileNotFoundException{
+    public static void startGame() throws FileNotFoundException{
         Scanner in = new Scanner(System.in);
-        Deck deck1 = new Deck();
-        boolean bScore=false;
-        int score=0;
+        Deck deck1 = new Deck(); //instances a new deck(cards matrix)
+        boolean bScore=false; //if true increases the score in the end of the round if cards match
+        int score=0; //game score, max 17 to win
         int round=1;
-        if(load == true){
-            //ler arquivos de carregamento
-        }
-        
-        for(int i=0;i<=500;i++){
-            round = i;
-            System.out.println(deck1);
+        for(int i=round;i<=500;i++){
+            System.out.println(deck1); //deck to string (\f in the start to clear the prompt
             System.out.println("Rodada nº " + i + "\nRestam " + (36-(score*2)) +" cartas.");
             
-            int c1, l1, c2, l2;
-            boolean verify = true;
+            int c1, l1, c2, l2; //column and lines variables, first and second card
+            boolean verify = true; //verify if the values can be played, otherwise need to play again
             
             //First card
             do{
                 do{
                     System.out.println("\nDigite o número da coluna. (1 a 6)");
-                    System.out.println("9 - Pausa");
-                    c1 = in.nextInt();
+                    System.out.println("9 - Pausa"); //only pauses in the first try to not compromise the looping
+                    c1 = in.nextInt(); //column1
                     if (c1 == 9) {
-                        boolean exit = pause(score, round);
+                        boolean exit = pause(); //boolean to return if true
                         if (exit == true) return;
                     }
                         
                 }while(c1<1 || c1>6);
                  do{
                     System.out.println("\nDigite o número da linha. (1 a 6)");
-                    l1 = in.nextInt();
+                    l1 = in.nextInt(); //line1
                 }while(l1<1 || l1>6);
-                verify = deck1.verifyValue(c1-1,l1-1);
+                verify = deck1.verifyValue(c1-1,l1-1); //must be -1 to decrease the index in  the matrix
                 if (verify == false) {
                     System.out.println("\n*Carta inválida, tente novamente*\n\n");
                 }
             }while(verify==false);
             
-            deck1.showCard(c1-1,l1-1);
+            deck1.showCard(c1-1,l1-1); //turn the card
             System.out.println(deck1);
             System.out.println("Rodada nº " + i + "\nRestam " + (36-(score*2)) +" cartas.");
             
@@ -82,11 +75,11 @@ public class GameApp{
             do{
                 do{
                     System.out.println("\nDigite o número da coluna. (1 a 6)");
-                    c2 = in.nextInt();
+                    c2 = in.nextInt(); //card2column
                 }while(c2<1 || c2>6);
                 do{
                     System.out.println("\nDigite o número da linha. (1 a 6)");
-                    l2 = in.nextInt();
+                    l2 = in.nextInt(); //card2line
                 }while(l2<1 || l2>6);
                 verify = deck1.verifyValue(c2-1,l2-1);
                 if (verify == false) {
@@ -98,7 +91,7 @@ public class GameApp{
             System.out.println(deck1);
             System.out.println("Rodada nº " + i + "\nRestam " + (36-(score*2)) +" cartas.");
             
-            Utilities.waitTurn(1500);
+            Utilities.waitTurn(1500); // after show the cards give a time to see them and turn back
             bScore = deck1.compareCards(c1-1, l1-1, c2-1, l2-1); //boolean return of compare cards
             if (bScore == true) { score += 1; }
             bScore = false;
@@ -117,13 +110,13 @@ public class GameApp{
         
     }
     
-    public static boolean pause(int score, int round) throws FileNotFoundException{
+    public static boolean pause(){
         Scanner in = new Scanner(System.in);
         int option = 0, option1 = 0;
         do{
-            System.out.println("\n1 - Salvar\n2 - Sair\n3 - Voltar");
+            System.out.println("\n2 - Sair\n3 - Voltar");
             option = in.nextInt();
-            LoadNSave.save(score, round);
+            //if(option == 1) ;//metodo salvar
             if(option == 2) {
                 do{
                     System.out.println("Deseja realmente sair?\n1 - Sim\n2 - Voltar");
